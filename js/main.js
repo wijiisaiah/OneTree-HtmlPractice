6c92bcad43b96cc0018c43491a3741687558e406
Isaiah Wijaya <wijiisaiah@gmail.com>
2017-04-05 10:19:13 -0700

    Smooth scroll, animations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
 /**
  * Created by Isaiah on 2017-04-03.
  */
+var $ = jQuery;
+
+'use strict';
+
+$(document).ready(function () {
+    console.log($(window).scrollTop());
+    $(window).scroll(function () {
+        // console.log($(window).scrollTop());
+
+        var h = window.innerHeight;
+
+        if ($(window).scrollTop() > h) {
+            $('.navbar').addClass('fixed-top');
+        }
+
+        if ($(window).scrollTop() < h) {
+            console.log("In scroll top");
+            $('.navbar').removeClass('fixed-top');
+        }
+
+    });
+    
+    $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+            e.preventDefault();
+            var target = this.hash;
+            $target = $(target);
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 700, 'swing', function () {
+                window.location.hash = target;
+            });
+        });
+    
+    $('.js-wp-1').waypoint(function(direction){
+        $('.js-wp-1').addClass('animated fadeInDown');
+    }, {
+        offset: '90%'
+    })
+
+});
